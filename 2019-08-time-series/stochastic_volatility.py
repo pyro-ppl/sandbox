9dbaad57f56eaa7554d705940cb122daf2391402
JP Chen <jpchen@uber.com>
2019-08-09 16:02:27 -0700

    resolve shape bug

--- 2019-08-time-series/stochastic_volatility.py
+++ 2019-08-time-series/stochastic_volatility.py
@@ -9,7 +9,7 @@ from torch.distributions import constraints
 
 import pyro
 import pyro.distributions as dist
-from pyro.infer.autoguide import AutoDelta
+from pyro.infer.autoguide import AutoDelta, AutoMultivariateNormal
 from pyro.infer import SVI, JitTraceEnum_ELBO, Trace_ELBO
 from pyro.optim import Adam
 
@@ -18,7 +18,7 @@ logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.IN
 
 """
 Multivariate Stochastic Volatility Model
-See Model 2 in https://pdfs.semanticscholar.org/fccc/6f4ee933d4330eabf377c08f8b2650e1f244.pdf
+See Model 2 on page 365 in https://pdfs.semanticscholar.org/fccc/6f4ee933d4330eabf377c08f8b2650e1f244.pdf
 """
 def model(data):
     """
@@ -46,15 +46,16 @@ def model(data):
 
         L_eta = pyro.param('L_eta', torch.eye(hidden_dim), constraint=constraints.lower_cholesky)
         mu_eta = torch.zeros(hidden_dim)
-        trans_matrix = pyro.param('phi', torch.ones(hidden_dim))
-        trans_matrix = torch.diag(trans_matrix)
-        trans_dist = dist.MultivariateNormal(mu_eta, scale_tril=L_eta).expand((timesteps,))
+        trans_matrix = pyro.param('phi', torch.ones(hidden_dim, hidden_dim))
+        # this gives us a zero matrix with phi on the diagonal
+        trans_matrix = trans_matrix.diag().diag()
+        trans_dist = dist.MultivariateNormal(mu_eta, scale_tril=L_eta)# .expand((timesteps,))
 
         mu_gamma  = pyro.param('mu_gamma', torch.zeros(obs_dim))
         L_gamma = pyro.param('sigma_gamma', torch.eye(obs_dim), constraint=constraints.lower_cholesky)
         obs_matrix = torch.eye(hidden_dim, obs_dim)
         # latent state is h_t - mu
-        obs_dist = dist.MultivariateNormal(-mu_gamma, scale_tril=L_gamma).expand((timesteps,))
+        obs_dist = dist.MultivariateNormal(-mu_gamma, scale_tril=L_gamma)# .expand((timesteps,))
 
         pyro.sample('obs', dist.GaussianHMM(init_dist, trans_matrix, trans_dist, obs_matrix, obs_dist), obs=data)
 
@@ -72,7 +73,8 @@ def sequential_model(num_samples=10, timesteps=100, hidden_dim=4, obs_dim=2):
     cov = torch.eye(hidden_dim, hidden_dim)
     mu_obs = torch.zeros(obs_dim)
     cov_obs = torch.eye(obs_dim, obs_dim)
-    transition = torch.randn(num_samples, hidden_dim, hidden_dim)
+    transition = 0.2 * torch.randn(hidden_dim, hidden_dim)
+    transition = transition.diag().diag().expand(num_samples, -1, -1)
     trans_dist = dist.MultivariateNormal(mu, cov).expand((num_samples,))
     obs_dist = dist.MultivariateNormal(mu_obs, cov_obs).expand((num_samples,))
     z = torch.zeros(num_samples, hidden_dim)
@@ -93,12 +95,12 @@ def sequential_model(num_samples=10, timesteps=100, hidden_dim=4, obs_dim=2):
 def main(args):
     pyro.enable_validation(True)
     pyro.set_rng_seed(123)
-    # generate data
+    # generate synthetic data
     data = sequential_model()
-    print(data.shape)
+    logging.debug(data.shape)
     # MAP estimation
     guide = AutoDelta(model)
-    svi = SVI(model, guide, Adam({'lr': 1e-3}), Trace_ELBO())
+    svi = SVI(model, guide, Adam({'lr': 1e-2}), Trace_ELBO())
     for i in range(args.num_epochs):
         loss = svi.step(data)
         logging.info(loss)
@@ -110,6 +112,6 @@ def main(args):
 if __name__ == "__main__":
     assert pyro.__version__.startswith('0.3.4')
     parser = argparse.ArgumentParser(description="Stochastic volatility")
-    parser.add_argument("-n", "--num-epochs", default=100, type=int)
+    parser.add_argument("-n", "--num-epochs", default=200, type=int)
     args = parser.parse_args()
     main(args)
