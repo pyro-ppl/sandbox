8e4473d64da9823f874bc1572862a4c06c205b0b
Martin Jankowiak <jankowiak@uber.com>
2020-03-05 09:12:02 -0800

    cleanup

--- 2020-03-forecasting/multivariate.py
+++ 2020-03-forecasting/multivariate.py
@@ -16,6 +16,7 @@ from pyro.contrib.timeseries import IndependentMaternGP, LinearlyCoupledMaternGP
 from pyro.nn import PyroParam
 from pyro.infer.reparam import SymmetricStableReparam
 from pyro.infer.reparam import LinearHMMReparam
+from pyro.poutine import reparam
 
 from os.path import exists
 from urllib.request import urlopen
@@ -33,15 +34,14 @@ class IndependentMaternStableProcess(IndependentMaternGP):
                  obs_noise_scale_init=None):
         super().__init__(nu=nu, dt=dt, obs_dim=obs_dim, length_scale_init=length_scale_init,
                          kernel_scale_init=kernel_scale_init, obs_noise_scale_init=obs_noise_scale_init)
-        self.min_stability = 1.5
-        self.stability = PyroParam(torch.tensor(1.99 - self.min_stability), constraint=constraints.positive)
+        # self.stability = PyroParam(torch.tensor(1.99 - self.min_stability), constraint=constraints.positive)
 
     def _get_init_dist(self):
         return dist.MultivariateNormal(self.obs_matrix.new_zeros(self.obs_dim, self.kernel.state_dim),
                                        self.kernel.stationary_covariance().squeeze(-3))
 
     def _get_obs_dist(self):
-        return dist.Stable(self.min_stability + self.stability, 0.0,
+        return dist.Stable(1.90, 0.0,
                            scale=self.obs_noise_scale.unsqueeze(-1).unsqueeze(-1)).to_event(1)
 
     def _get_trans_dist(self, trans_matrix, stationary_covariance):
@@ -56,6 +56,7 @@ class IndependentMaternStableProcess(IndependentMaternGP):
         trans_matrix = trans_matrix.unsqueeze(-3)
         return dist.LinearHMM(self._get_init_dist(), trans_matrix, trans_dist,
                               self.obs_matrix, self._get_obs_dist(), duration=duration)
+
 
 def preprocess(args):
     """
@@ -101,33 +102,33 @@ class Model(ForecastingModel):
         if self.noise_model in ["ind", "stable"]:
             noise_dist = dist.IndependentHMM(noise_dist)
 
-        rep = LinearHMMReparam(obs=SymmetricStableReparam())
-        with pyro.poutine.reparam(config={"residual": rep}):
+        config = {"residual": LinearHMMReparam(obs=SymmetricStableReparam())} if self.noise_model == "stable" \
+                 else {}
+
+        with reparam(config=config):
             self.predict(noise_dist, zero_data)
 
-        #self.predict(noise_dist, zero_data)
 
 def main(args):
     print(args)
     print("")
 
     pyro.enable_validation(__debug__)
+    pyro.set_rng_seed(args.seed)
+
     data, covariates = preprocess(args)
 
-    print("data",data.shape)
-    print("covariates",covariates.shape)
+    print("data:", data.shape)
+    print("covariates:", covariates.shape)
 
-    # The backtest() function automatically trains and evaluates our model on
-    # different windows of data.
     def forecaster_options(t0, t1, t2):
-        num_steps = args.num_steps if t1==args.train_window else 0
-        _forecaster_options = {
-            "num_steps": num_steps,
-            "learning_rate": args.learning_rate,
-            "learning_rate_decay": args.learning_rate_decay,
-            "log_every": args.log_every,
-            "dct_gradients": args.dct,
-            "warm_start": True}
+        num_steps = args.num_steps if t1==args.train_window else 50
+        lr = args.learning_rate if t1==args.train_window else 0.1 * args.learning_rate
+        lrd = args.learning_rate_decay if t1==args.train_window else 1.0
+        _forecaster_options = {"num_steps": num_steps, "learning_rate": lr,
+                               "learning_rate_decay": lrd, "log_every": args.log_every,
+                               "dct_gradients": args.dct, "warm_start": True,
+                               "clip_norm": args.clip_norm}
         return _forecaster_options
 
     metrics = backtest(data, covariates, lambda: Model(noise_model=args.noise_model),
@@ -138,11 +139,13 @@ def main(args):
                        num_samples=args.num_samples,
                        forecaster_options=forecaster_options)
 
+    print("### EVALUATION ###")
     for name in ["mae", "rmse", "crps"]:
         values = [m[name] for m in metrics]
         mean = np.mean(values)
         std = np.std(values)
         print("{} = {:0.3g} +- {:0.3g}".format(name, mean, std))
+
     return metrics
 
 
@@ -150,15 +153,16 @@ if __name__ == "__main__":
     #assert pyro.__version__.startswith('1.2.1')
     parser = argparse.ArgumentParser(description="Multivariate timeseries models")
     parser.add_argument("--noise-model", default='stable', type=str, choices=['ind', 'lc', 'stable'])
-    parser.add_argument("--train-window", default=500, type=int)
+    parser.add_argument("--train-window", default=600, type=int)
     parser.add_argument("--test-window", default=1, type=int)
     parser.add_argument("--stride", default=1, type=int)
-    parser.add_argument("-n", "--num-steps", default=800, type=int)
+    parser.add_argument("--clip-norm", default=5.0, type=float)
+    parser.add_argument("-n", "--num-steps", default=600, type=int)
     parser.add_argument("-lr", "--learning-rate", default=0.01, type=float)
     parser.add_argument("-lrd", "--learning-rate-decay", default=0.01, type=float)
     parser.add_argument("--dct", action="store_true")
     parser.add_argument("--num-samples", default=100, type=int)
-    parser.add_argument("--log-every", default=5, type=int)
-    parser.add_argument("--seed", default=1234567890, type=int)
+    parser.add_argument("--log-every", default=20, type=int)
+    parser.add_argument("--seed", default=0, type=int)
     args = parser.parse_args()
     main(args)
