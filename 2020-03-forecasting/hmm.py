a80e18c21520522a11532e8956a912637af46bf5
Martin Jankowiak <jankowiak@uber.com>
2020-03-12 11:19:19 -0700

    add studentT

--- 2020-03-forecasting/hmm.py
+++ 2020-03-forecasting/hmm.py
@@ -6,23 +6,14 @@ import uuid
 
 import numpy as np
 import math
-import pandas as pd
-
 import torch
 import torch.distributions.constraints as constraints
 
 import pyro
-import pyro.distributions as dist
-from pyro.contrib.forecast import ForecastingModel, backtest, HMCForecaster, Forecaster
-from pyro.contrib.timeseries import IndependentMaternGP
-from pyro.nn import PyroParam, PyroSample, PyroModule
-from pyro.infer.reparam import SymmetricStableReparam, LatentStableReparam, StudentTReparam
-from pyro.infer.reparam import LinearHMMReparam
-from pyro.poutine import reparam
-from pyro.distributions import MultivariateNormal, LogNormal, Uniform, StudentT
-
-from os.path import exists
-from urllib.request import urlopen
+from pyro.contrib.forecast import ForecastingModel, backtest, Forecaster
+from pyro.nn import PyroParam, PyroModule
+from pyro.infer.reparam import SymmetricStableReparam, StudentTReparam, LinearHMMReparam
+from pyro.distributions import StudentT, Stable, Normal, LinearHMM
 
 import pickle
 from dataloader import get_data as get_raw_data
@@ -38,8 +29,8 @@ class StableLinearHMM(PyroModule):
         self.obs_noise = obs_noise
         self.state_dim = state_dim
         self.obs_dim = obs_dim
-        assert trans_noise in ['gaussian', 'stable']
-        assert obs_noise in ['gaussian', 'stable']
+        assert trans_noise in ['gaussian', 'stable', 'student']
+        assert obs_noise in ['gaussian', 'stable', 'student']
         super().__init__()
         self.obs_noise_scale = PyroParam(0.2 * torch.tensor(obs_dim), constraint=constraints.positive)
         self.trans_noise_scale = PyroParam(0.2 * torch.tensor(state_dim), constraint=constraints.positive)
@@ -47,28 +38,37 @@ class StableLinearHMM(PyroModule):
         self.obs_matrix = PyroParam(0.3 * torch.randn(state_dim, obs_dim))
         if trans_noise == "stable":
             self.trans_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+        elif trans_noise == "student":
+            self.trans_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
         if obs_noise == "stable":
             self.obs_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+        elif obs_noise == "student":
+            self.obs_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
 
     def _get_init_dist(self):
-        return dist.Normal(torch.zeros(self.state_dim), torch.ones(self.state_dim)).to_event(1)
+        return Normal(torch.zeros(self.state_dim), torch.ones(self.state_dim)).to_event(1)
 
     def _get_obs_dist(self):
         if self.obs_noise == "stable":
-            return dist.Stable(self.obs_stability, torch.zeros(self.obs_dim), scale=self.obs_noise_scale / root_two).to_event(1)
+            return Stable(self.obs_stability, torch.zeros(self.obs_dim),
+                          scale=self.obs_noise_scale / root_two).to_event(1)
+        elif self.obs_noise == "student":
+            return StudentT(self.obs_nu, torch.zeros(self.obs_dim), self.obs_noise_scale).to_event(1)
         else:
-            return dist.Normal(torch.zeros(self.obs_dim), scale=self.obs_noise_scale).to_event(1)
+            return Normal(torch.zeros(self.obs_dim), scale=self.obs_noise_scale).to_event(1)
 
     def _get_trans_dist(self):
         if self.trans_noise == "stable":
-            return dist.Stable(self.trans_stability, torch.zeros(self.state_dim), scale=self.trans_noise_scale / root_two).to_event(1)
+            return Stable(self.trans_stability, torch.zeros(self.state_dim),
+                          scale=self.trans_noise_scale / root_two).to_event(1)
+        elif self.trans_noise == "student":
+            return StudentT(self.trans_nu, torch.zeros(self.state_dim), self.trans_noise_scale).to_event(1)
         else:
-            return dist.Normal(torch.zeros(self.state_dim), scale=self.trans_noise_scale).to_event(1)
+            return Normal(torch.zeros(self.state_dim), scale=self.trans_noise_scale).to_event(1)
 
     def get_dist(self, duration=None):
-        return dist.LinearHMM(self._get_init_dist(),
-                              self.trans_matrix, self._get_trans_dist(),
-                              self.obs_matrix, self._get_obs_dist(), duration=duration)
+        return LinearHMM(self._get_init_dist(), self.trans_matrix, self._get_trans_dist(),
+                         self.obs_matrix, self._get_obs_dist(), duration=duration)
 
 
 def get_data(args=None):
@@ -103,11 +103,17 @@ class Model(ForecastingModel):
             self.config = {"residual": LinearHMMReparam(obs=SymmetricStableReparam())}
         elif trans_noise == "stable" and obs_noise == "stable":
             self.config = {"residual": LinearHMMReparam(obs=SymmetricStableReparam(), trans=SymmetricStableReparam())}
+        elif trans_noise == "gaussian" and obs_noise == "student":
+            self.config = {"residual": LinearHMMReparam(obs=StudentTReparam())}
+        elif trans_noise == "student" and obs_noise == "gaussian":
+            self.config = {"residual": LinearHMMReparam(trans=StudentTReparam())}
+        elif trans_noise == "student" and obs_noise == "student":
+            self.config = {"residual": LinearHMMReparam(obs=StudentTReparam(), trans=StudentTReparam())}
 
     def model(self, zero_data, covariates):
         hmm = self.hmm.get_dist(duration=zero_data.size(-2))
 
-        with reparam(config=self.config):
+        with pyro.poutine.reparam(config=self.config):
             self.predict(hmm, zero_data)
 
 
@@ -129,9 +135,9 @@ def main(**args):
     pyro.set_rng_seed(args['seed'])
 
     def svi_forecaster_options(t0, t1, t2):
-        num_steps = args['num_steps']# if t1 == args.train_window else 200
-        lr = args['learning_rate'] # if t1 == args.train_window else 0.1 * args.learning_rate
-        lrd = args['learning_rate_decay']# if t1 == args.train_window else 0.1
+        num_steps = args['num_steps']  # if t1 == args.train_window else 200
+        lr = args['learning_rate']  # if t1 == args.train_window else 0.1 * args.learning_rate
+        lrd = args['learning_rate_decay']  # if t1 == args.train_window else 0.1
         return {"num_steps": num_steps, "learning_rate": lr,
                 "learning_rate_decay": lrd, "log_every": args['log_every'],
                 "dct_gradients": False, "warm_start": False,
@@ -184,14 +190,14 @@ def main(**args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Multivariate timeseries models")
-    parser.add_argument("--trans-noise", default='stable', type=str, choices=['gaussian', 'stable'])
-    parser.add_argument("--obs-noise", default='stable', type=str, choices=['gaussian', 'stable'])
+    parser.add_argument("--trans-noise", default='stable', type=str, choices=['gaussian', 'stable', 'student'])
+    parser.add_argument("--obs-noise", default='stable', type=str, choices=['gaussian', 'stable', 'student'])
     parser.add_argument("--dataset", default='metals', type=str)
     parser.add_argument("--data-dir", default='./data/', type=str)
     parser.add_argument("--log-dir", default='./logs/', type=str)
-    parser.add_argument("--train-window", default=300, type=int)
+    parser.add_argument("--train-window", default=100, type=int)
     parser.add_argument("--test-window", default=5, type=int)
-    parser.add_argument("--num-windows", default=5, type=int)
+    parser.add_argument("--num-windows", default=2, type=int)
     parser.add_argument("--stride", default=5, type=int)
     parser.add_argument("--num-eval-samples", default=200, type=int)
     parser.add_argument("--clip-norm", default=20.0, type=float)
