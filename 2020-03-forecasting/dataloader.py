d293772f1f782b814889fb6e5a707f1d04769155
Martin Jankowiak <jankowiak@uber.com>
2020-03-12 09:58:52 -0700

    update hmm

---
+++ 2020-03-forecasting/dataloader.py
@@ -0,0 +1,131 @@
+import numpy as np
+import torch
+from os.path import exists
+from urllib.request import urlopen
+
+
+def download_data():
+    if not exists("eeg.dat"):
+        url = "http://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff"
+        with open("eeg.dat", "wb") as f:
+            f.write(urlopen(url).read())
+
+
+def get_data(dataset, datadir):
+    if dataset=='eeg':
+        download_data()
+        data = np.loadtxt(datadir + '/eeg.dat', delimiter=',', skiprows=19)
+        print("[raw data shape] {}".format(data.shape))
+        #data = data[::2, :]
+        #data = data[:4800, :]
+        data = data[::20, :]
+        data = torch.tensor(data[:, :-1]).float()
+        print("[data shape after thinning] {}".format(data.shape))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+    elif dataset == 'currency':
+        # https://datahub.io/core/exchange-rates
+        data = torch.tensor(np.load(datadir + '/currency.npy'))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[currency data shape] {}".format(data.shape))
+    elif dataset in ['krakeu', 'krakusd', 'short']:
+        # http://www.cryptodatadownload.com/
+        if dataset=='short':
+            clip, dataset = True, 'krakeu'
+        else:
+            clip = False
+        data = torch.tensor(np.load(datadir + '/' + dataset + '.npy'))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        if clip:
+            data = data[0:100, :]
+        print("[{} data shape] {}".format(dataset, data.shape))
+    elif dataset == 'ercot':
+        # https://blog.valohai.com/smart-grids-use-machine-learning-to-forecast-load
+        data = np.loadtxt(datadir + '/ercot.csv', skiprows=1, delimiter=',', usecols=(1,2,3,4,5,10))
+        data = torch.tensor(data[0:2000, :])
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+    elif dataset == 'house':
+        # https://archive.ics.uci.edu/ml/datasets/individual+household+electric+power+consumption#
+        data = np.loadtxt(datadir + '/house.txt',skiprows=1,delimiter=';',usecols=(2,3,4,5),max_rows=2000)
+        data = torch.tensor(data)
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+    elif dataset == 'aq':
+        # https://archive.ics.uci.edu/ml/datasets/Air+quality
+        data = np.loadtxt(datadir + '/aq.csv', delimiter=';', skiprows=1,
+                          usecols=(3,6,7,8,9,10,11,12,13,14))
+                          #usecols = (2,3,4,5,6,7,8,9,10,11,12,13,14))
+        data = torch.tensor(data[0:749, :])
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[aq data shape] {}".format(data.shape))
+        return data
+    elif dataset in ['ushum', 'ustemp']:
+        # https://www.kaggle.com/selfishgene/historical-hourly-weather-data/download
+        data = torch.tensor(np.load(datadir + '/%s.npy' % dataset))
+        data = data[-2000:, :]
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[{} data shape] {}".format(dataset, data.shape))
+    elif dataset == 'colo':
+        # http://coagmet.colostate.edu/station_map.php
+        data = torch.tensor(np.load(datadir + '/colotemp.npy'))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[colorado temperature data shape] {}".format(data.shape))
+    elif dataset in ['open', 'volume']:
+        # https://www.kaggle.com/jessevent/all-crypto-currencies
+        data = torch.tensor(np.load(datadir + '/crypto_%s.npy' % dataset))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[crypto {} data shape] {}".format(dataset, data.shape))
+    elif dataset=='metals':
+        # https://www.amark.com/graphs/silver-spot-prices
+        data = torch.tensor(np.load(datadir + '/metals.npy'))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+    elif dataset=='dow':
+        # https://fred.stlouisfed.org/categories/32255/downloaddata
+        data = torch.tensor(np.load(datadir + '/dow.npy'))
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+    elif dataset == 'sml':
+        # https://archive.ics.uci.edu/ml/datasets/SML2010
+        data = np.loadtxt(datadir + '/sml.txt', delimiter=' ', skiprows=1,
+                          usecols = (2,3,5,6,7,8,9,10,12,13,14,15,16,17,21,22))
+        #data = torch.tensor(data[0:3600, :])
+        data = torch.tensor(data[0:2000, :])
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+        print("[sml data shape] {}".format(data.shape))
+    elif dataset in ['oak', 'sf']:
+        data = torch.tensor(np.load(datadir + '/%scounts.npy' % dataset))
+        data = data.reshape(data.size(0) // 24, 24, -1).sum(-2)
+        data = data[0:2919, 0:8]
+        data = data.reshape(data.size(0) // 7, 7, -1).sum(-2)
+        print("[{} count data shape] {}".format(dataset, data.shape))
+        for station in range(8):
+            assert data.shape[0] == np.count_nonzero(data[:, station])
+        data = data.log()
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = data - data_mean
+        #data = (data - data_mean) / data_std
+    elif dataset=='synth':
+        D = 10
+        omegas = 0.05 * (1.0 + torch.rand(D))
+        ts = torch.arange(3600).unsqueeze(-1)
+        data = torch.cos(omegas * ts) + 0.03 * torch.randn(ts.size(0), D)
+        data_mean, data_std = data.mean(0), data.std(0)
+        data = (data - data_mean) / data_std
+
+
+    T = data.size(0)
+    T_train = int(0.8 * T)
+    #T_train = int(0.8 * T) if dataset != 'eeg' else 4800
+    #T_train = int(0.8 * T) if dataset != 'eeg' else 600
+    T_val = (T - T_train) // 2
+    T_test = T - T_train - T_val
+    return data, T_train, T_val, T_test
--- 2020-03-forecasting/hmm.py
+++ 2020-03-forecasting/hmm.py
@@ -45,21 +45,23 @@ class StableLinearHMM(PyroModule):
         self.trans_noise_scale = PyroParam(0.2 * torch.tensor(state_dim), constraint=constraints.positive)
         self.trans_matrix = PyroParam(0.3 * torch.randn(state_dim, state_dim))
         self.obs_matrix = PyroParam(0.3 * torch.randn(state_dim, obs_dim))
-        if trans_noise == "stable" or obs_noise == "stable":
-            self.stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+        if trans_noise == "stable":
+            self.trans_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+        if obs_noise == "stable":
+            self.obs_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
 
     def _get_init_dist(self):
         return dist.Normal(torch.zeros(self.state_dim), torch.ones(self.state_dim)).to_event(1)
 
     def _get_obs_dist(self):
         if self.obs_noise == "stable":
-            return dist.Stable(self.stability, torch.zeros(self.obs_dim), scale=self.obs_noise_scale / root_two).to_event(1)
+            return dist.Stable(self.obs_stability, torch.zeros(self.obs_dim), scale=self.obs_noise_scale / root_two).to_event(1)
         else:
             return dist.Normal(torch.zeros(self.obs_dim), scale=self.obs_noise_scale).to_event(1)
 
     def _get_trans_dist(self):
         if self.trans_noise == "stable":
-            return dist.Stable(self.stability, torch.zeros(self.state_dim), scale=self.trans_noise_scale / root_two).to_event(1)
+            return dist.Stable(self.trans_stability, torch.zeros(self.state_dim), scale=self.trans_noise_scale / root_two).to_event(1)
         else:
             return dist.Normal(torch.zeros(self.state_dim), scale=self.trans_noise_scale).to_event(1)
 
@@ -69,9 +71,10 @@ class StableLinearHMM(PyroModule):
                               self.obs_matrix, self._get_obs_dist(), duration=duration)
 
 
-def get_data(args=None, num_windows=2):
+def get_data(args=None):
     data, _, _, _ = get_raw_data(args['dataset'], args['data_dir'])
-    to_keep = args['train_window'] + num_windows * args['test_window']
+
+    to_keep = args['train_window'] + args['num_windows'] * args['test_window']
     assert to_keep <= data.size(0)
 
     data = data[:to_keep].float()
@@ -109,9 +112,9 @@ class Model(ForecastingModel):
 
 
 def main(**args):
-    log_file = '{}.{}.{}.tt_{}_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.{}.log'
+    log_file = '{}.{}.{}.tt_{}_{}.nw_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.{}.log'
     log_file = log_file.format(args['dataset'], args['trans_noise'], args['obs_noise'],
-                               args['train_window'], args['test_window'],
+                               args['train_window'], args['test_window'], args['num_windows'],
                                args['state_dim'], args['num_steps'],
                                args['clip_norm'], args['learning_rate'], args['learning_rate_decay'],
                                str(uuid.uuid4())[0:4])
@@ -155,12 +158,25 @@ def main(**args):
                        forecaster_fn=Forecaster)
 
     log("### EVALUATION ###")
-    for name in ["mae", "rmse", "crps"]:
+    for name in ["mae", "crps"]:
         values = [m[name] for m in metrics]
         mean, std = np.mean(values), np.std(values)
         results[name] = mean
         results[name + '_std'] = std
         log("{} = {:0.4g} +- {:0.4g}".format(name, mean, std))
+    for name in ["mae_fine", "crps_fine"]:
+        values = np.stack([m[name] for m in metrics])
+        results[name] = values
+        for t in range(values.shape[1]):
+            mean = np.mean(values[:, t, :])
+            std = np.std(values[:, t, :])
+            metric_t = name[:-5] + '_{}'.format(t + 1)
+            results[metric_t] = mean
+            results[metric_t + '_std'] = std
+            log("{} = {:0.4g} +- {:0.4g}".format(metric_t, mean, std))
+
+    pred = np.stack([m['pred'].data.cpu().numpy() for m in metrics])
+    results['pred'] = pred
 
     with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
         pickle.dump(results, f, protocol=2)
@@ -170,16 +186,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Multivariate timeseries models")
     parser.add_argument("--trans-noise", default='stable', type=str, choices=['gaussian', 'stable'])
     parser.add_argument("--obs-noise", default='stable', type=str, choices=['gaussian', 'stable'])
-    parser.add_argument("--dataset", default='krakeu', type=str)
+    parser.add_argument("--dataset", default='metals', type=str)
     parser.add_argument("--data-dir", default='./data/', type=str)
     parser.add_argument("--log-dir", default='./logs/', type=str)
     parser.add_argument("--train-window", default=300, type=int)
     parser.add_argument("--test-window", default=5, type=int)
+    parser.add_argument("--num-windows", default=5, type=int)
     parser.add_argument("--stride", default=5, type=int)
-    parser.add_argument("--num-eval-samples", default=2000, type=int)
+    parser.add_argument("--num-eval-samples", default=200, type=int)
     parser.add_argument("--clip-norm", default=20.0, type=float)
-    parser.add_argument("-n", "--num-steps", default=10, type=int)
-    parser.add_argument("-d", "--state-dim", default=3, type=int)
+    parser.add_argument("-n", "--num-steps", default=3, type=int)
+    parser.add_argument("-d", "--state-dim", default=2, type=int)
     parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
     parser.add_argument("-lrd", "--learning-rate-decay", default=0.005, type=float)
     parser.add_argument("--plot", action="store_true")
