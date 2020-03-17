5ce34df5d099ab38b0d0112ff68b56fa45a72f82
Martin Jankowiak <jankowiak@uber.com>
2020-03-16 17:14:10 -0700

    change solar.py to use hmm

--- 2020-03-forecasting/solar.py
+++ 2020-03-forecasting/solar.py
@@ -12,90 +12,87 @@ import torch.distributions.constraints as constraints
 
 import pyro
 from pyro.contrib.forecast import ForecastingModel, backtest, Forecaster
-from pyro.nn import PyroParam
-from pyro.infer.reparam import SymmetricStableReparam, StudentTReparam, StableReparam, LocScaleReparam
-from pyro.distributions import StudentT, Stable, Normal
-from pyro.ops.tensor_utils import periodic_cumsum, periodic_features, periodic_repeat
-import pyro.poutine as poutine
+from pyro.nn import PyroParam, PyroModule
+from pyro.infer.reparam import SymmetricStableReparam, StudentTReparam, LinearHMMReparam, StableReparam
+from pyro.distributions import StudentT, Stable, Normal, LinearHMM
 
 import pickle
 from dataloader import get_data as get_raw_data
 from logger import get_logger
+from pyro.ops.tensor_utils import periodic_cumsum, periodic_features, periodic_repeat
 
 
 root_two = math.sqrt(2.0)
 
 
-class Model(ForecastingModel):
-    def __init__(self, obs_noise="gaussian", trans_noise="gaussian"):
-        super().__init__()
-        self.obs_noise = obs_noise
+class StableLinearHMM(PyroModule):
+    def __init__(self, obs_dim=1, trans_noise="gaussian", obs_noise="gaussian", state_dim=3):
         self.trans_noise = trans_noise
-
-        if obs_noise == "gaussian":
-            self.obs_config = {}
-        elif obs_noise == "stable":
-            self.obs_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
-            self.obs_config = {"residual": SymmetricStableReparam()}
-        elif obs_noise == "student":
-            self.obs_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
-            self.obs_config = {"residual": StudentTReparam()}
-
-        if trans_noise in ["gaussian", "none"]:
-            self.trans_config = {}
-        elif trans_noise == "stable":
+        self.obs_noise = obs_noise
+        self.state_dim = state_dim
+        self.obs_dim = obs_dim
+        print("Initilized StableLinearHMM with state_dim = {}, obs_dim = {}".format(state_dim, obs_dim))
+        assert trans_noise in ['gaussian', 'stable', 'student', 'skew']
+        assert obs_noise in ['gaussian', 'stable', 'student', 'skew']
+        super().__init__()
+        self.obs_noise_scale = PyroParam(0.2 * torch.ones(obs_dim), constraint=constraints.positive)
+        self.trans_noise_scale = PyroParam(0.2 * torch.ones(state_dim), constraint=constraints.positive)
+        self.trans_matrix = PyroParam(0.3 * torch.randn(state_dim, state_dim))
+        self.obs_matrix = PyroParam(0.3 * torch.randn(state_dim, obs_dim))
+        if trans_noise in ["stable", "skew"]:
             self.trans_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
-            self.trans_config = {"drift": SymmetricStableReparam()}
+            if trans_noise == "skew":
+                self.trans_skew = PyroParam(torch.tensor(0.0), constraint=constraints.interval(-0.99, 0.99))
         elif trans_noise == "student":
             self.trans_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
-            self.trans_config = {"drift": StudentTReparam()}
+        if obs_noise in ["stable", "skew"]:
+            self.obs_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+            if obs_noise == "skew":
+                self.obs_skew = PyroParam(torch.tensor(0.0), constraint=constraints.interval(-0.99, 0.99))
+        elif obs_noise == "student":
+            self.obs_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
 
-    def model(self, zero_data, covariates):
-        feature_dim = covariates.size(-1)
-        duration, num_stations = zero_data.shape
+    def _get_init_dist(self):
+        return Normal(torch.zeros(self.state_dim), torch.ones(self.state_dim)).to_event(1)
 
-        seasonality_weights = pyro.sample("seasonality_weights",
-                                          Normal(0, 0.1).expand([feature_dim, num_stations]).to_event(2))
-
-        periodic = torch.einsum('...fs,tf->...ts', seasonality_weights, covariates)
-        if periodic.dim() > 3:
-            periodic = periodic.squeeze(-3)
-
-        obs_scale = pyro.param("obs_scale", 0.2 * torch.ones(num_stations), constraint=constraints.positive)
-        trans_scale = pyro.param("trans_scale", 1.0e-4 * torch.ones(num_stations), constraint=constraints.positive)
-        station_bias = pyro.param("station_bias", torch.zeros(num_stations))
-
-        with self.time_plate, poutine.reparam(config={"drift": LocScaleReparam()}):
-            with poutine.reparam(config=self.trans_config):
-                if self.trans_noise == "gaussian":
-                    drift = pyro.sample("drift", Normal(torch.zeros(zero_data.shape), trans_scale).to_event(1))
-                elif self.trans_noise == "stable":
-                    drift = pyro.sample("drift", Stable(self.trans_stability,
-                                                        0.0, scale=trans_scale / root_two).to_event(1))
-                elif self.trans_noise == "student":
-                    drift = pyro.sample("drift", StudentT(self.trans_nu, 0.0, scale=trans_scale).to_event(1))
-
-        if self.trans_noise != "none":
-            prediction = periodic + station_bias + drift.cumsum(-2)
-        else:
-            prediction = periodic + station_bias
-
-        if self.obs_noise == "gaussian":
-            noise_dist = Normal(0, obs_scale)
-        elif self.obs_noise == "stable":
-            noise_dist = Stable(self.obs_stability, 0.0, scale=obs_scale / root_two)
+    def _get_obs_dist(self, obs_scale=None):
+        if self.obs_noise == "stable":
+            return Stable(self.obs_stability, torch.zeros(self.obs_dim),
+                          scale=self.obs_noise_scale / root_two).to_event(1)
+        elif self.obs_noise == "skew":
+            return Stable(self.obs_stability, self.obs_skew,
+                          scale=self.obs_noise_scale / root_two).to_event(1)
         elif self.obs_noise == "student":
-            noise_dist = StudentT(self.obs_nu, 0.0, scale=obs_scale)
+            return StudentT(self.obs_nu, torch.zeros(self.obs_dim), self.obs_noise_scale).to_event(1)
+        else:
+            #var = self.obs_noise_scale.pow(2.0) + hetero_var
+            #ratio = hetero_var.mean(0) / var.mean(0)
+            #print("var ratio", ratio.data.cpu().numpy().tolist())
+            return Normal(torch.zeros(self.obs_dim), scale=obs_scale).to_event(1)
+            #return Normal(torch.zeros(self.obs_dim), scale=var.sqrt()).to_event(1)
 
-        with pyro.poutine.reparam(config=self.obs_config):
-            self.predict(noise_dist, prediction)
+    def _get_trans_dist(self):
+        if self.trans_noise == "stable":
+            return Stable(self.trans_stability, torch.zeros(self.state_dim),
+                          scale=self.trans_noise_scale / root_two).to_event(1)
+        elif self.trans_noise == "skew":
+            return Stable(self.trans_stability, self.trans_skew,
+                          scale=self.trans_noise_scale / root_two).to_event(1)
+        elif self.trans_noise == "student":
+            return StudentT(self.trans_nu, torch.zeros(self.state_dim), self.trans_noise_scale).to_event(1)
+        else:
+            return Normal(torch.zeros(self.state_dim), scale=self.trans_noise_scale).to_event(1)
+
+    def get_dist(self, duration=None, obs_scale=None):
+        return LinearHMM(self._get_init_dist(), self.trans_matrix, self._get_trans_dist(),
+                         self.obs_matrix, self._get_obs_dist(obs_scale), duration=duration)
 
 
 def get_data(args=None):
     data, _, _, _ = get_raw_data(args['dataset'], args['data_dir'])
     print("raw data", data.shape)
 
-    to_keep = args['train_window'] + args['test_window']
+    to_keep = args['train_window'] + args['num_windows'] * args['test_window']
     assert to_keep <= data.size(0)
 
     data = data[:to_keep].float()
@@ -105,15 +102,76 @@ def get_data(args=None):
     data_std = data.std(0)
     data /= data_std
 
-    covariates = periodic_features(data.size(0), 24 * 60, 10)
+    #long_covariates = periodic_features(data.size(0), 365 * 24 * 60, 30 * 24 * 60)
+    #long_covariates = periodic_features(data.size(0), 365 * 24 * 60, 30 * 24 * 60)
+    short_covariates = periodic_features(data.size(0), 24 * 60, 10)
+    #print("long_covariates",long_covariates.shape)
+    #covariates = torch.cat([short_covariates, long_covariates], dim=1)
+    covariates = short_covariates
+    print("covariates", covariates.shape)
 
     return data.cuda(), covariates.cuda()
 
 
+class Model(ForecastingModel):
+    def __init__(self, trans_noise="gaussian", obs_noise="gaussian", state_dim=3, obs_dim=14):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.trans_noise = trans_noise
+        self.obs_noise = obs_noise
+        self.hmm = StableLinearHMM(obs_dim=obs_dim, trans_noise=trans_noise, obs_noise=obs_noise, state_dim=state_dim)
+        trans, obs = None, None
+        self.long_feature_dim = 0
+
+        if trans_noise == "stable":
+            trans = SymmetricStableReparam()
+        elif trans_noise == "skew":
+            trans = StableReparam()
+        elif trans_noise == "student":
+            trans = StudentTReparam()
+        if obs_noise == "stable":
+            obs = SymmetricStableReparam()
+        elif obs_noise == "skew":
+            obs = StableReparam()
+        elif obs_noise == "student":
+            obs = StudentTReparam()
+
+        self.config = {"residual": LinearHMMReparam(obs=obs, trans=trans)}
+
+    def model(self, zero_data, covariates):
+        feature_dim = covariates.size(-1)
+        long_feature_dim = self.long_feature_dim
+        short_feature_dim = feature_dim - long_feature_dim
+        short_covariates = covariates[:, :short_feature_dim]
+        #long_covariates = covariates[:, short_feature_dim:]
+
+        hour = torch.arange(24).repeat(covariates.size(0) // 24)
+
+        daily_weights = pyro.sample("daily_weights", Normal(0, 0.1).expand([short_feature_dim, self.obs_dim]).to_event(2))
+        #daily_weights2 = pyro.sample("daily_weights2", Normal(0, 0.1).expand([short_feature_dim, self.obs_dim]).to_event(2))
+        #monthly_weights = pyro.sample("monthly_weights", Normal(0, 0.1).expand([long_feature_dim, self.obs_dim]).to_event(2))
+
+        obs_scale = pyro.param("obs_scale", 0.2 * torch.ones(24, zero_data.size(-1)), constraint=constraints.positive)
+        obs_scale = obs_scale.index_select(0, hour)
+
+        daily_periodic = torch.einsum('...fs,tf->...ts', daily_weights, short_covariates)
+        #daily_periodic2 = torch.einsum('...fs,tf->...ts', daily_weights2, short_covariates)
+        #monthly_periodic = torch.einsum('...fs,tf->...ts', monthly_weights, long_covariates)
+        periodic = daily_periodic #+ monthly_periodic#.unsqueeze(-1)
+        if periodic.dim() > 3:
+            periodic = periodic.squeeze(-3)
+            #daily_periodic2 = daily_periodic2.squeeze(-3)
+
+        hmm = self.hmm.get_dist(duration=zero_data.size(-2), obs_scale=obs_scale)#, hetero_var=torch.nn.functional.softplus(daily_periodic2 - 6.0))
+
+        with pyro.poutine.reparam(config=self.config):
+            self.predict(hmm, periodic)
+
+
 def main(**args):
-    log_file = '{}.{}.{}.tt_{}_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.seed_{}.{}.log'
+    log_file = '{}.{}.{}.tt_{}_{}.nw_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.seed_{}.{}.log'
     log_file = log_file.format(args['dataset'], args['trans_noise'], args['obs_noise'],
-                               args['train_window'], args['test_window'],
+                               args['train_window'], args['test_window'], args['num_windows'],
                                args['state_dim'], args['num_steps'],
                                args['clip_norm'], args['learning_rate'], args['learning_rate_decay'],
                                args['seed'],
@@ -121,7 +179,7 @@ def main(**args):
 
     log = get_logger(args['log_dir'], log_file, use_local_logger=False)
 
-    # torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    #torch.set_default_tensor_type('torch.cuda.FloatTensor')
     torch.set_default_tensor_type('torch.cuda.DoubleTensor')
 
     log(args)
@@ -144,21 +202,26 @@ def main(**args):
                 "num_particles": 1}
 
     data, covariates = get_data(args=args)
-    print("covariates", covariates.shape)
-
     results = {}
 
     metrics = backtest(data, covariates,
-                       lambda: Model(obs_noise=args['obs_noise'], trans_noise=args['trans_noise']).cuda(),
+                       lambda: Model(trans_noise=args['trans_noise'], state_dim=args['state_dim'],
+                                     obs_noise=args['obs_noise'], obs_dim=data.size(-1)).cuda(),
                        train_window=None,
                        seed=args['seed'],
                        min_train_window=args['train_window'],
                        test_window=args['test_window'],
                        stride=args['stride'],
                        num_samples=args['num_eval_samples'],
-                       batch_size=5,
+                       batch_size=2,
                        forecaster_options=svi_forecaster_options,
                        forecaster_fn=Forecaster)
+
+    num_eval_windows = (args['num_windows'] - 1) * args['test_window'] + 1
+    pyro.set_rng_seed(0)
+    index = torch.randperm(num_eval_windows)
+    index_test = index[:math.ceil(0.80 * num_eval_windows)].data.cpu().numpy()
+    index_val = index[math.ceil(0.80 * num_eval_windows):].data.cpu().numpy()
 
     log("### EVALUATION ###")
     for name in ["mae", "crps"]:
@@ -167,9 +230,32 @@ def main(**args):
         results[name] = mean
         results[name + '_std'] = std
         log("{} = {:0.4g} +- {:0.4g}".format(name, mean, std))
+    for name in []:
+        values = np.stack([m[name] for m in metrics])
+        results[name] = values
+        for t in range(values.shape[1]):
+            metric_t = name[:-5] + '_{}'.format(t + 1)
 
-    # pred = np.stack([m['pred'].data.cpu().numpy() for m in metrics])
-    # results['pred'] = pred
+            mean = np.mean(values[:, t, :])
+            std = np.std(values[:, t, :])
+            results[metric_t] = mean
+            results[metric_t + '_std'] = std
+            log("{} = {:0.4g} +- {:0.4g}".format(metric_t, mean, std))
+
+            mean = np.mean(values[index_val, t, :])
+            std = np.std(values[index_val, t, :])
+            results[metric_t + '_val'] = mean
+            results[metric_t + '_val_std'] = std
+            log("{} = {:0.4g} +- {:0.4g}".format(metric_t + '_val', mean, std))
+
+            mean = np.mean(values[index_test, t, :])
+            std = np.std(values[index_test, t, :])
+            results[metric_t + '_test'] = mean
+            results[metric_t + '_test_std'] = std
+            log("{} = {:0.4g} +- {:0.4g}".format(metric_t + '_test', mean, std))
+
+    #pred = np.stack([m['pred'].data.cpu().numpy() for m in metrics])
+    #results['pred'] = pred
 
     for name, value in pyro.get_param_store().items():
         if value.numel() == 1:
@@ -179,31 +265,31 @@ def main(**args):
             results[name] = value.data.cpu().numpy()
             print("[{}]".format(name), value.data.cpu().numpy())
 
-    with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
-        pickle.dump(results, f, protocol=2)
+    #with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
+    #    pickle.dump(results, f, protocol=2)
 
     log("[ELAPSED TIME]: {:.3f}".format(time.time() - t0))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Multivariate timeseries models")
-    parser.add_argument("--trans-noise", default='stable', type=str,
-                        choices=['gaussian', 'stable', 'student', 'skew', 'none'])
-    parser.add_argument("--obs-noise", default='stable', type=str, choices=['gaussian', 'stable', 'student', 'skew'])
+    parser.add_argument("--trans-noise", default='gaussian', type=str, choices=['gaussian', 'stable', 'student', 'skew'])
+    parser.add_argument("--obs-noise", default='gaussian', type=str, choices=['gaussian', 'stable', 'student', 'skew'])
     parser.add_argument("--dataset", default='solar', type=str)
     parser.add_argument("--data-dir", default='./data/', type=str)
     parser.add_argument("--log-dir", default='./logs/', type=str)
-    parser.add_argument("--train-window", default=250000, type=int)
-    parser.add_argument("--test-window", default=50000, type=int)
-    parser.add_argument("--stride", default=50000, type=int)
-    parser.add_argument("--num-eval-samples", default=200, type=int)
+    parser.add_argument("--train-window", default=92 * 24 * 60, type=int)
+    parser.add_argument("--test-window", default=30 * 24 * 60, type=int)
+    parser.add_argument("--num-windows", default=1, type=int)
+    parser.add_argument("--stride", default=1, type=int)
+    parser.add_argument("--num-eval-samples", default=120, type=int)
     parser.add_argument("--clip-norm", default=10.0, type=float)
-    parser.add_argument("-n", "--num-steps", default=300, type=int)
-    parser.add_argument("-d", "--state-dim", default=5, type=int)
+    parser.add_argument("-n", "--num-steps", default=800, type=int)
+    parser.add_argument("-d", "--state-dim", default=6, type=int)
     parser.add_argument("-lr", "--learning-rate", default=0.03, type=float)
-    parser.add_argument("-lrd", "--learning-rate-decay", default=0.001, type=float)
+    parser.add_argument("-lrd", "--learning-rate-decay", default=0.003, type=float)
     parser.add_argument("--plot", action="store_true")
-    parser.add_argument("--log-every", default=20, type=int)
+    parser.add_argument("--log-every", default=5, type=int)
     parser.add_argument("--seed", default=0, type=int)
     args = parser.parse_args()
 
---
+++ 2020-03-forecasting/solar_old.py
@@ -0,0 +1,218 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import uuid
+import time
+
+import numpy as np
+import math
+import torch
+import torch.distributions.constraints as constraints
+
+import pyro
+from pyro.contrib.forecast import ForecastingModel, backtest, Forecaster
+from pyro.nn import PyroParam
+from pyro.infer.reparam import SymmetricStableReparam, StudentTReparam, StableReparam, LocScaleReparam
+from pyro.distributions import StudentT, Stable, Normal
+from pyro.ops.tensor_utils import periodic_cumsum, periodic_features, periodic_repeat, convolve
+import pyro.poutine as poutine
+
+import pickle
+from dataloader import get_data as get_raw_data
+from logger import get_logger
+
+
+root_two = math.sqrt(2.0)
+
+
+class Model(ForecastingModel):
+    def __init__(self, obs_noise="gaussian", trans_noise="gaussian"):
+        super().__init__()
+        self.obs_noise = obs_noise
+        self.trans_noise = trans_noise
+
+        if obs_noise == "gaussian":
+            self.obs_config = {}
+        elif obs_noise == "stable":
+            self.obs_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+            self.obs_config = {"residual": SymmetricStableReparam()}
+        elif obs_noise == "student":
+            self.obs_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
+            self.obs_config = {"residual": StudentTReparam()}
+
+        if trans_noise in ["gaussian", "none"]:
+            self.trans_config = {}
+        elif trans_noise == "stable":
+            self.trans_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
+            self.trans_config = {"drift": SymmetricStableReparam()}
+        elif trans_noise == "student":
+            self.trans_nu = PyroParam(torch.tensor(5.0), constraint=constraints.interval(1.01, 30.0))
+            self.trans_config = {"drift": StudentTReparam()}
+
+    def model(self, zero_data, covariates):
+        feature_dim = covariates.size(-1)
+        duration, num_stations = zero_data.shape
+
+        seasonality_weights = pyro.sample("seasonality_weights",
+                                          Normal(0, 0.1).expand([feature_dim, num_stations]).to_event(2))
+
+        periodic = torch.einsum('...fs,tf->...ts', seasonality_weights, covariates)
+        if periodic.dim() > 3:
+            periodic = periodic.squeeze(-3)
+
+        obs_scale = pyro.param("obs_scale", 0.2 * torch.ones(num_stations), constraint=constraints.positive)
+        trans_scale = pyro.param("trans_scale", 1.0e-4 * torch.ones(num_stations), constraint=constraints.positive)
+        #trans_scale = pyro.param("trans_scale", torch.tensor(1.0e-1), constraint=constraints.positive)
+        station_bias = pyro.param("station_bias", torch.zeros(num_stations))
+
+        with self.time_plate, poutine.reparam(config={"drift": LocScaleReparam()}):
+            with poutine.reparam(config=self.trans_config):
+                if self.trans_noise == "gaussian":
+                    drift = pyro.sample("drift", Normal(torch.zeros(zero_data.shape), trans_scale).to_event(1))
+                elif self.trans_noise == "stable":
+                    drift = pyro.sample("drift", Stable(self.trans_stability, torch.zeros(zero_data.shape),
+                                                        scale=trans_scale / root_two).to_event(1))
+                elif self.trans_noise == "student":
+                    drift = pyro.sample("drift", StudentT(self.trans_nu, torch.zeros(zero_data.shape), scale=trans_scale).to_event(1))
+
+        #print("drift", drift.shape)
+        timescale = pyro.param("timescale", 10.0 * torch.ones(num_stations), constraint=constraints.positive)
+        time = torch.arange(float(duration))
+        kernel = time.div(timescale.unsqueeze(-1)).neg().exp()
+        kernel = kernel / kernel.sum()
+        motion = convolve(drift.transpose(-1, -2), kernel, mode="same").transpose(-1, -2)
+
+        if self.trans_noise != "none":
+            prediction = periodic + station_bias + motion
+        else:
+            prediction = periodic + station_bias + motion
+
+        if self.obs_noise == "gaussian":
+            noise_dist = Normal(0, obs_scale)
+        elif self.obs_noise == "stable":
+            noise_dist = Stable(self.obs_stability, 0.0, scale=obs_scale / root_two)
+        elif self.obs_noise == "student":
+            noise_dist = StudentT(self.obs_nu, 0.0, scale=obs_scale)
+
+        with pyro.poutine.reparam(config=self.obs_config):
+            self.predict(noise_dist, prediction)
+
+
+def get_data(args=None):
+    data, _, _, _ = get_raw_data(args['dataset'], args['data_dir'])
+    print("raw data", data.shape)
+
+    to_keep = args['train_window'] + args['test_window']
+    assert to_keep <= data.size(0)
+
+    data = data[:to_keep].float()
+
+    data_mean = data.mean(0)
+    data -= data_mean
+    data_std = data.std(0)
+    data /= data_std
+
+    covariates = periodic_features(data.size(0), 24 * 60, 10)
+
+    return data.cuda(), covariates.cuda()
+
+
+def main(**args):
+    log_file = '{}.{}.{}.tt_{}_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.seed_{}.{}.log'
+    log_file = log_file.format(args['dataset'], args['trans_noise'], args['obs_noise'],
+                               args['train_window'], args['test_window'],
+                               args['state_dim'], args['num_steps'],
+                               args['clip_norm'], args['learning_rate'], args['learning_rate_decay'],
+                               args['seed'],
+                               str(uuid.uuid4())[0:4])
+
+    log = get_logger(args['log_dir'], log_file, use_local_logger=False)
+
+    # torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    torch.set_default_tensor_type('torch.cuda.DoubleTensor')
+
+    log(args)
+    log("")
+
+    pyro.enable_validation(__debug__)
+    pyro.set_rng_seed(args['seed'])
+
+    t0 = time.time()
+
+    def svi_forecaster_options(t0, t1, t2):
+        num_steps = args['num_steps']  # if t1 == args.train_window else 200
+        lr = args['learning_rate']  # if t1 == args.train_window else 0.1 * args.learning_rate
+        lrd = args['learning_rate_decay']  # if t1 == args.train_window else 0.1
+        return {"num_steps": num_steps, "learning_rate": lr,
+                "learning_rate_decay": lrd, "log_every": args['log_every'],
+                "dct_gradients": True, "warm_start": False,
+                "clip_norm": args['clip_norm'],
+                "vectorize_particles": False,
+                "num_particles": 1}
+
+    data, covariates = get_data(args=args)
+    print("covariates", covariates.shape)
+
+    results = {}
+
+    metrics = backtest(data, covariates,
+                       lambda: Model(obs_noise=args['obs_noise'], trans_noise=args['trans_noise']).cuda(),
+                       train_window=None,
+                       seed=args['seed'],
+                       min_train_window=args['train_window'],
+                       test_window=args['test_window'],
+                       stride=args['stride'],
+                       num_samples=args['num_eval_samples'],
+                       batch_size=6,
+                       forecaster_options=svi_forecaster_options,
+                       forecaster_fn=Forecaster)
+
+    log("### EVALUATION ###")
+    for name in ["mae", "crps"]:
+        values = [m[name] for m in metrics]
+        mean, std = np.mean(values), np.std(values)
+        results[name] = mean
+        results[name + '_std'] = std
+        log("{} = {:0.4g} +- {:0.4g}".format(name, mean, std))
+
+    # pred = np.stack([m['pred'].data.cpu().numpy() for m in metrics])
+    # results['pred'] = pred
+
+    for name, value in pyro.get_param_store().items():
+        if value.numel() == 1:
+            results[name] = value.item()
+            print("[{}]".format(name), value.item())
+        elif value.numel() < 10:
+            results[name] = value.data.cpu().numpy()
+            print("[{}]".format(name), value.data.cpu().numpy())
+
+    with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
+        pickle.dump(results, f, protocol=2)
+
+    log("[ELAPSED TIME]: {:.3f}".format(time.time() - t0))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Multivariate timeseries models")
+    parser.add_argument("--trans-noise", default='stable', type=str,
+                        choices=['gaussian', 'stable', 'student', 'skew', 'none'])
+    parser.add_argument("--obs-noise", default='gaussian', type=str, choices=['gaussian', 'stable', 'student', 'skew'])
+    parser.add_argument("--dataset", default='solar', type=str)
+    parser.add_argument("--data-dir", default='./data/', type=str)
+    parser.add_argument("--log-dir", default='./logs/', type=str)
+    parser.add_argument("--train-window", default=365 * 24 * 60, type=int)
+    parser.add_argument("--test-window", default=30 * 24 * 60, type=int)
+    parser.add_argument("--stride", default=50000, type=int)
+    parser.add_argument("--num-eval-samples", default=120, type=int)
+    parser.add_argument("--clip-norm", default=10.0, type=float)
+    parser.add_argument("-n", "--num-steps", default=500, type=int)
+    parser.add_argument("-d", "--state-dim", default=5, type=int)
+    parser.add_argument("-lr", "--learning-rate", default=0.03, type=float)
+    parser.add_argument("-lrd", "--learning-rate-decay", default=0.001, type=float)
+    parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--log-every", default=20, type=int)
+    parser.add_argument("--seed", default=0, type=int)
+    args = parser.parse_args()
+
+    main(**vars(args))
