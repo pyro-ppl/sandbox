2b7b88365ef96a7e073bf6eda744fdeb59031e29
Martin Jankowiak <jankowiak@uber.com>
2020-03-18 12:41:17 -0700

    workaround cuda weirdness in solar.py

--- 2020-03-forecasting/solar.py
+++ 2020-03-forecasting/solar.py
@@ -23,6 +23,8 @@ from pyro.ops.tensor_utils import periodic_cumsum, periodic_features, periodic_r
 
 
 root_two = math.sqrt(2.0)
+num_stations = 5
+day = 24 * 60
 
 
 class StableLinearHMM(PyroModule):
@@ -31,14 +33,14 @@ class StableLinearHMM(PyroModule):
         self.obs_noise = obs_noise
         self.state_dim = state_dim
         self.obs_dim = obs_dim
-        print("Initilized StableLinearHMM with state_dim = {}, obs_dim = {}".format(state_dim, obs_dim))
+        print("Initialized StableLinearHMM with state_dim = {}, obs_dim = {}".format(state_dim, obs_dim))
         assert trans_noise in ['gaussian', 'stable', 'student', 'skew']
         assert obs_noise == 'gaussian'
         super().__init__()
-        self.obs_noise_scale = PyroParam(0.2 * torch.ones(obs_dim), constraint=constraints.positive)
+        #self.obs_noise_scale = PyroParam(0.2 * torch.ones(obs_dim), constraint=constraints.positive)
         self.trans_noise_scale = PyroParam(0.2 * torch.ones(state_dim), constraint=constraints.positive)
         self.trans_matrix = PyroParam(0.3 * torch.randn(state_dim, state_dim))
-        self.obs_matrix = PyroParam(0.3 * torch.randn(state_dim, obs_dim))
+        self.obs_matrix = PyroParam(0.3 * torch.randn(state_dim, num_stations))
         if trans_noise in ["stable", "skew"]:
             self.trans_stability = PyroParam(torch.tensor(1.95), constraint=constraints.interval(1.01, 1.99))
             if trans_noise == "skew":
@@ -56,17 +58,16 @@ class StableLinearHMM(PyroModule):
         return Normal(torch.zeros(self.state_dim), torch.ones(self.state_dim)).to_event(1)
 
     def _get_obs_dist(self, obs_scale=None):
-        assert obs_scale is not None
-        if self.obs_noise == "stable":
-            return Stable(self.obs_stability, torch.zeros(self.obs_dim),
-                          scale=self.obs_noise_scale / root_two).to_event(1)
-        elif self.obs_noise == "skew":
-            return Stable(self.obs_stability, self.obs_skew,
-                          scale=self.obs_noise_scale / root_two).to_event(1)
-        elif self.obs_noise == "student":
-            return StudentT(self.obs_nu, torch.zeros(self.obs_dim), self.obs_noise_scale).to_event(1)
-        else:
-            return Normal(torch.zeros(self.obs_dim), scale=obs_scale).to_event(1)
+        #if self.obs_noise == "stable":
+        #    return Stable(self.obs_stability, torch.zeros(self.obs_dim),
+        #                  scale=self.obs_noise_scale / root_two).to_event(1)
+        #elif self.obs_noise == "skew":
+        #    return Stable(self.obs_stability, self.obs_skew,
+        #                  scale=self.obs_noise_scale / root_two).to_event(1)
+        #elif self.obs_noise == "student":
+        #    return StudentT(self.obs_nu, torch.zeros(self.obs_dim), self.obs_noise_scale).to_event(1)
+        #else:
+        return Normal(torch.zeros(self.obs_dim), scale=obs_scale).to_event(1)
 
     def _get_trans_dist(self):
         if self.trans_noise == "stable":
@@ -81,8 +82,13 @@ class StableLinearHMM(PyroModule):
             return Normal(torch.zeros(self.state_dim), scale=self.trans_noise_scale).to_event(1)
 
     def get_dist(self, duration=None, obs_scale=None, obs_matrix_multiplier=None):
+        obs_matrix = self.obs_matrix.repeat_interleave(day, dim=-1)
+        print("obs_matrix", obs_matrix.shape)
+        print("obs_matrix_multiplier",obs_matrix_multiplier.shape)
+        print("obs_scale", obs_scale.shape)
+        print("duration", duration)
         return LinearHMM(self._get_init_dist(), self.trans_matrix, self._get_trans_dist(),
-                         obs_matrix_multiplier * self.obs_matrix, self._get_obs_dist(obs_scale), duration=duration)
+                         obs_matrix_multiplier * obs_matrix, self._get_obs_dist(obs_scale), duration=duration)
 
 
 def leftequal(x, delta):
@@ -108,10 +114,10 @@ def extract_night_features(data):
         darknext = darknext & rightequal(data, minute)
 
     dark = darklast | darknext
-    darkshift = np.concatenate([np.zeros(24 * 60, dtype=np.uint8), dark[:-24 * 60]])
-    darkshiftshift = np.concatenate([np.zeros(48 * 60, dtype=np.uint8), dark[:-48 * 60]])
+    darkshift = np.concatenate([np.zeros(day, dtype=np.uint8), dark[:-day]])
+    darkshiftshift = np.concatenate([np.zeros(2 * day, dtype=np.uint8), dark[:-2 * day]])
     dark = dark & darkshift & darkshiftshift
-    dark = np.concatenate([np.zeros(24 * 60, dtype=np.float32), dark[:-24 * 60]])
+    dark = np.concatenate([np.zeros(day, dtype=np.float32), dark[:-day]])
 
     return torch.tensor(dark)
 
@@ -124,20 +130,22 @@ def get_data(args=None):
     for dim in range(data.size(-1)):
         night_features[:, dim] = extract_night_features(data[:, dim])
 
-    to_drop = 194 * 24 * 60 - 1
-    to_keep = args['train_window'] + args['num_windows'] * args['test_window']
+    to_drop = 194 * day - 1
+    to_keep = day * (args['train_window'] + args['num_windows'] * args['test_window'])
     assert to_keep + to_drop <= data.size(0)
 
     data = data[to_drop:to_drop + to_keep].float()
+    covariates = periodic_features(data.size(0), day, 10)
+    data = data.reshape(data.size(0) // day, day * data.size(-1))
+
     night_features = night_features[to_drop:to_drop + to_keep].double()
+    night_features = night_features.reshape(night_features.size(0) // day, day * night_features.size(-1))
 
-    #data_mean = data.mean(0)
-    #data -= data_mean
-    #data_std = data.std(0)
-    #data /= data_std
-
-    covariates = periodic_features(data.size(0), 24 * 60, 10)
+    covariates = covariates.reshape(covariates.size(0) // day, day * covariates.size(-1))
+    print("periodic, night", covariates.shape, night_features.shape)
     covariates = torch.cat([night_features, covariates], dim=-1)
+
+    print("covariates, data", covariates.shape, data.shape)
 
     return data.cuda(), covariates.cuda()
 
@@ -151,6 +159,9 @@ class Model(ForecastingModel):
         self.nightval = nightval
         self.hmm = StableLinearHMM(obs_dim=obs_dim, trans_noise=trans_noise, obs_noise=obs_noise, state_dim=state_dim)
         trans, obs = None, None
+        self.mean_granularity = 24 * 6
+        self.night_scale = PyroParam(0.1 * torch.ones(num_stations), constraint=constraints.positive)
+        self.obs_scale = PyroParam(0.2 * torch.ones(self.mean_granularity, num_stations), constraint=constraints.positive)
 
         if trans_noise == "stable":
             trans = SymmetricStableReparam()
@@ -168,35 +179,45 @@ class Model(ForecastingModel):
         self.config = {"residual": LinearHMMReparam(obs=obs, trans=trans)}
 
     def model(self, zero_data, covariates):
-        short_covariates = covariates[:, self.obs_dim:]
         night_covariates = covariates[:, :self.obs_dim]
+        short_covariates = covariates[:, self.obs_dim:]
+        night_covariates = night_covariates.reshape(night_covariates.size(0) * day, num_stations)
+        short_covariates = short_covariates.reshape(short_covariates.size(0) * day, short_covariates.size(-1) // day)
+        print("short_covariates, night_covariates", short_covariates.shape, night_covariates.shape)
         day_covariates = 1.0 - night_covariates
+        T_minutes = short_covariates.size(0)
 
-        mean_granularity = 24 * 6
-        hour = torch.arange(mean_granularity).repeat(1 + covariates.size(0) // mean_granularity)[:covariates.size(0)]
-        daily_weights = pyro.sample("daily_weights", Normal(0, 0.1).expand([short_covariates.size(-1), self.obs_dim]).to_event(2))
+        hour = torch.arange(self.mean_granularity).repeat(1 + T_minutes // self.mean_granularity)[:T_minutes]
+        daily_weights = pyro.sample("daily_weights", Normal(0, 0.1).expand([short_covariates.size(-1), num_stations]).to_event(2))
+        print("daily_weights", daily_weights.shape)
 
-        night_scale = pyro.param("night_scale", 0.1 * torch.ones(zero_data.size(-1)), constraint=constraints.positive)
-
-        obs_scale = pyro.param("obs_scale", 0.2 * torch.ones(mean_granularity, zero_data.size(-1)), constraint=constraints.positive)
-        obs_scale = obs_scale.index_select(0, hour)
-        obs_scale = day_covariates * obs_scale + night_covariates * night_scale
+        obs_scale = self.obs_scale.index_select(0, hour)
+        print("obs_scale", obs_scale.shape)
+        obs_scale = day_covariates * obs_scale + night_covariates * self.night_scale
+        obs_scale = obs_scale.reshape(obs_scale.size(0) // day, self.obs_dim)
+        print("obs_scale2", obs_scale.shape)
 
         periodic = torch.einsum('...fs,tf->...ts', daily_weights, short_covariates)
         if periodic.dim() > 3:
             periodic = periodic.squeeze(-3)
+        print("periodic", periodic.shape)
 
         periodic = day_covariates * periodic + night_covariates * self.nightval
+        print("periodic", periodic.shape)
+        periodic = periodic.reshape(periodic.shape[:-2] + (periodic.size(-2) // day, self.obs_dim))
+        print("periodic2", periodic.shape)
+        obs_matrix_multiplier = day_covariates.reshape(day_covariates.size(0) // day, self.obs_dim).unsqueeze(-2)
+        print("obs_matrix_multiplier", obs_matrix_multiplier.shape)
 
         hmm = self.hmm.get_dist(duration=zero_data.size(-2), obs_scale=obs_scale,
-                                obs_matrix_multiplier=day_covariates.unsqueeze(-2))
+                                obs_matrix_multiplier=obs_matrix_multiplier)
 
         with pyro.poutine.reparam(config=self.config):
             self.predict(hmm, periodic)
 
 
 def main(**args):
-    torch.cuda.set_device(0)
+    #torch.cuda.set_device(0)
     log_file = '{}.{}.{}.tt_{}_{}.nw_{}.sd_{}.nst_{}.cn_{:.1f}.lr_{:.2f}.lrd_{:.2f}.seed_{}.{}.log'
     log_file = log_file.format(args['dataset'], args['trans_noise'], args['obs_noise'],
                                args['train_window'], args['test_window'], args['num_windows'],
@@ -208,6 +229,7 @@ def main(**args):
     log = get_logger(args['log_dir'], log_file, use_local_logger=False)
 
     #torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    #torch.set_default_tensor_type('torch.DoubleTensor')
     torch.set_default_tensor_type('torch.cuda.DoubleTensor')
 
     log(args)
@@ -241,7 +263,7 @@ def main(**args):
                        test_window=args['test_window'],
                        stride=args['stride'],
                        num_samples=args['num_eval_samples'],
-                       batch_size=2,
+                       batch_size=1,
                        forecaster_options=svi_forecaster_options,
                        forecaster_fn=Forecaster)
 
@@ -293,8 +315,8 @@ def main(**args):
             results[name] = value.data.cpu().numpy()
             print("[{}]".format(name), value.data.cpu().numpy())
 
-    with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
-        pickle.dump(results, f, protocol=2)
+    #with open(args['log_dir'] + '/' + log_file[:-4] + '.pkl', 'wb') as f:
+    #    pickle.dump(results, f, protocol=2)
 
     log("[ELAPSED TIME]: {:.3f}".format(time.time() - t0))
 
@@ -306,11 +328,11 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", default='solar', type=str)
     parser.add_argument("--data-dir", default='./data/', type=str)
     parser.add_argument("--log-dir", default='./logs/', type=str)
-    parser.add_argument("--train-window", default=92 * 24 * 60, type=int)
-    parser.add_argument("--test-window", default=24 * 60, type=int)
+    parser.add_argument("--train-window", default=92, type=int)
+    parser.add_argument("--test-window", default=1, type=int)
     parser.add_argument("--num-windows", default=1, type=int)
-    parser.add_argument("--stride", default=24 * 60, type=int)
-    parser.add_argument("--num-eval-samples", default=500, type=int)
+    parser.add_argument("--stride", default=day, type=int)
+    parser.add_argument("--num-eval-samples", default=10, type=int)
     parser.add_argument("--clip-norm", default=10.0, type=float)
     parser.add_argument("-n", "--num-steps", default=1000, type=int)
     parser.add_argument("-d", "--state-dim", default=6, type=int)
